569a5c2583af0aaf09424a98806fe7f7dc4769b4
yashgupta-112 <85554972+yashgupta-112@users.noreply.github.com>
2022-06-17 16:18:16 +0530

    heroku production

--- backend/settings.py
+++ backend/settings.py
@@ -137,6 +137,10 @@ import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
